8d476363e624aded347840767f805597fa1696cd
neooffline <admin@neooffline.ru>
2019-03-14 12:21:04 +0300

    1 Задание

---
+++ src/ru/neooffline/homework_j2l5/ArrayMath.java
@@ -0,0 +1,51 @@
+package ru.neooffline.homework_j2l5;
+
+public class ArrayMath extends Thread {
+    static final int size = 10000000;
+    static final int h = size / 2;
+    float[] arr = new float[size];
+
+    public void oneTreadMath() {
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5)
+                    + Math.cos(0.5f + i / 2));
+        }
+        System.out.printf("Время, затраченно без деления "System.currentTimeMillis() - a);
+    }
+
+    public void twoTheadmath() {
+        long a,aRes;
+        float[] arr1 = new float[h];
+        float[] arr2 = new float[h];
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+        a = System.currentTimeMillis();
+        System.arraycopy(arr, 0, arr1, 0, h);
+        System.arraycopy(arr, h, arr2, 0, h);
+        aRes = System.currentTimeMillis() - a;
+        a = System.currentTimeMillis();
+        for (int i = 0; i < h; i++) {
+            arr1[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5)
+                    + Math.cos(0.5f + i / 2));
+        }
+        aRes += System.currentTimeMillis() - a;
+        a = System.currentTimeMillis();
+        for (int i = 0; i < h; i++) {
+            arr1[i] = (float) (arr2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5)
+                    + Math.cos(0.5f + i / 2));
+        }
+        aRes += System.currentTimeMillis() - a;
+        a = System.currentTimeMillis();
+        System.arraycopy(arr1, 0, arr, 0, h);
+        System.arraycopy(arr2, 0, arr, h, h);
+        aRes += System.currentTimeMillis() - a;
+        System.out.println(aRes);
+    }
+
+
+}
--- src/ru/neooffline/homework_j2l5/Main.java
+++ src/ru/neooffline/homework_j2l5/Main.java
@@ -3,6 +3,8 @@ package ru.neooffline.homework_j2l5;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+	ArrayMath arrayMath = new ArrayMath();
+	arrayMath.oneTreadMath();
+	arrayMath.twoTheadmath();
     }
 }
